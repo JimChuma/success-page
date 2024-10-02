3c9927da615e8c441e9c2d049940b241a7819689
JimChuma <85954599+JimChuma@users.noreply.github.com>
2024-10-02 15:57:51 +0100

    Complete Page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import logo from "./image/285278905_407543031517554_1762956712224382902_n - Copy.png";
+import fb from "./image/facebook-svgrepo-com (4).svg";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div>
+        <img src={logo} alt="logo" />
+        <span>Jim's Art</span>
+      </div>
+      <div>
+        <h4>Kindly send us a message on Facebook Messenger</h4>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Send us a message with your <b>design details, pictures and receipt url </b>
+          so our graphic designers can get started on your work. Our delivery
+          time is within 0 -48 hours. Our designers will respond to your
+          messages as soon as they can.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+        <p>Thank you! 😊</p>
+        <a href="https://m.me/JimsArtUSA" target="blank">
+          <button>
+            <img src={fb} alt="Facebook logo" />
+            Facebook Messenger
+          </button>
         </a>
-      </header>
+      </div>
     </div>
   );
 }
